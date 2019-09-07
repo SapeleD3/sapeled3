c7fa9f312a3a2fb76c91fc91c7bbe45c0f7ede72
sapeleD3 <oyinkuromosesvictor@outlook.com>
2019-09-07 19:10:14 +0100

    Updates

---
+++ precache-manifest.7b2501ca1aba2b783efc9cea02161d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113c820926efbd08d3b0c0797fe28397",
+    "url": "/sapeled3/index.html"
+  },
+  {
+    "revision": "816556a75518b0e8fc2d",
+    "url": "/sapeled3/static/css/main.a3b48102.chunk.css"
+  },
+  {
+    "revision": "3fe11345f64eabbdd752",
+    "url": "/sapeled3/static/js/2.01b60587.chunk.js"
+  },
+  {
+    "revision": "816556a75518b0e8fc2d",
+    "url": "/sapeled3/static/js/main.23987dde.chunk.js"
+  },
+  {
+    "revision": "aabdc4fbedfc8b2e78ea",
+    "url": "/sapeled3/static/js/runtime~main.6d6de2b7.js"
+  },
+  {
+    "revision": "3fc68260f94deb44f8d5967ddf4d3e18",
+    "url": "/sapeled3/static/media/mine.3fc68260.jpeg"
+  }
+]);
